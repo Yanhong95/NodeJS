c6df80435337020455df8bc702a36732254018cd
Yanhong95 <yanhongmain@gmail.com>
2020-11-14 18:11:19 -0500

    hide composeEnhancers

--- Burger_builder_full_stack/Client/src/index.js
+++ Burger_builder_full_stack/Client/src/index.js
@@ -12,17 +12,20 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
     auth: authReducer
 });
 
-const store = createStore(rootReducer, composeEnhancers(
+const store = createStore(rootReducer, 
     applyMiddleware(thunk)
-));
+);
+
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const store = createStore(rootReducer, composeEnhancers(
+//     applyMiddleware(thunk)
+// ));
 
 const app = (
     <Provider store={store}>
